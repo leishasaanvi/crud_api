b494fef926ab9c872967464eda3e202735d0aa32
leishasaanvi <leishasaanvi@gmail.com>
2024-08-13 11:04:56 +0530

    intefrated chuck norris jokes api (version1)

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
-/*config = {}
-
-
-config.database = process.env.LOCALHOST_URL
-
-
-module.exports = config*/
-
+/*
 const dotenv = require('dotenv');
 const path = require('path');
 
@@ -18,6 +11,26 @@ const config = {};
 
 config.database = process.env.MONGO_CONNECTION_STRING;
 console.log("Config:", config.database)
+
+// Export the config object
+module.exports = config;*/
+
+
+const dotenv = require('dotenv');
+const path = require('path');
+
+// Load environment variables from the .env file
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+// Log the MongoDB connection string to ensure it's loaded correctly
+console.log(`MongoDB Connection String: ${process.env.MONGO_CONNECTION_STRING}`);
+
+const config = {
+  database: process.env.MONGO_CONNECTION_STRING
+};
+
+// Log the config object to ensure it's correctly assigned
+console.log("Config:", config.database);
 
 // Export the config object
 module.exports = config;
--- index.js
+++ index.js
@@ -1,35 +1,5 @@
-/*const express = require("express")
-const cors = require("cors")
-const mongoose = require("mongoose")
-const config = require('./config')
 
-
-
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-
-//Import routes
-const userRoutes = require('./app/users.routes.js')
-
-//Configure routes
-app.use('/api/users', userRoutes)
-
-
-//Connect to database
-mongoose.connect(config.database)
-.then(() => console.log('Database connection established!'))
-.catch(() => console.log('Database connection failed!'))
-
-app.get('/', (req, res) => {
-    res.send('<h1>CRUD API</h1>')
-})
-
-const port = 3000
-
-app.listen(port, () => console.log(`Server is running at ${port} !`))*/
-
+/*
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
@@ -43,6 +13,7 @@ app.use(cors());
 // Import and configure routes
 const userRoutes = require('./app/users.routes.js');
 app.use('/api/users', userRoutes);
+
 
 // Connect to the database
 mongoose.connect("mongodb+srv://leishasaanvi:gn7f6mGxlDQC3axk@workspacedb.wiwgp.mongodb.net/?retryWrites=true&w=majority&appName=WorkspaceDB", {
@@ -60,3 +31,58 @@ app.get('/', (req, res) => {
 const port = 3000;
 
 app.listen(port, () => console.log(`Server is running at ${port}!`));
+*/
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const config = require('./config');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+// Import and configure user routes
+const userRoutes = require('./app/users.routes.js');
+app.use('/api/users', userRoutes);
+
+// Route for fetching jokes
+app.get('/api/jokes', (req, res) => {
+    let config = {
+        method: 'get',
+        maxBodyLength: Infinity,
+        url: 'https://api.chucknorris.io/jokes/random',
+        headers: { }
+    };
+
+    axios.request(config)
+    .then((response) => {
+        res.status(200).json(response.data);
+    })
+    .catch((error) => {
+        console.error(error);
+        res.status(500).send("Unable to fetch joke!");
+    });
+});
+
+// Connect to the database
+mongoose.connect(config.database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+.then(() => console.log('Database connection established!'))
+.catch(err => console.error('Database connection failed!', err));
+
+// Test route
+app.get('/', (req, res) => {
+    res.send('<h1>CRUD API with Jokes</h1>');
+});
+
+const port = 3000;
+
+app.listen(port, () => console.log(`Server is running at ${port}!`));
+
+
+
